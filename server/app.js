a17210dca02893a35ce5b5168f09e8be021617cd
dxmxnlord <rishikesh.sriram06@gmail.com>
2019-12-28 05:48:26 +0530

    feat(Auth): added the authentication for the  endpoint for login and the middleware for the api endpoints authentication

--- server/app.js
+++ server/app.js
@@ -3,8 +3,21 @@ const graphqlHTTP = require('express-graphql')
 const app = express()
 const schema = require('./graphql/schema/schema')
 const logger = require('winston')
+const passport = require('passport')
+const bodyParser = require('body-parser')
+const Cors = require('cors')
+const auth_router = require('./routes/auth.routes')
+const api_router = require('./routes/api.routes')
+const auth_middleware = require('./middleware/auth.middleware')
 
+require('./passport/passport')
 
+app.use(Cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(passport.initialize())
+app.use('/auth',auth_router)
+app.use('/api',auth_middleware.jwt_auth,api_router)
 
 app.use(
    '/graphql',
---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,15 @@
+const passport = require("passport")
+const logger = require('../winston')
+
+exports.jwt_auth = (req,res,next) => {
+	passport.authenticate('jwt',{ session: false }, function(err,user,info){
+		if(err){
+			logger.error(err.message)
+			return next(err)
+		} else if (!user) {
+			return res.json(info)
+		} else {
+			return next()
+		}
+	})(req,res,next)
+}
