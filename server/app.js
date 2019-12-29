5c840d96c5431b0712f46daaf79fefe9073200f4
dxmxnlord <rishikesh.sriram06@gmail.com>
2019-12-30 01:03:47 +0530

    fix(App): consistency fix and made port number an environment variable

--- server/app.js
+++ server/app.js
@@ -5,14 +5,16 @@ const schema = require('./graphql/schema/schema')
 const logger = require('winston')
 const passport = require('passport')
 const bodyParser = require('body-parser')
-const Cors = require('cors')
+const cors = require('cors')
 const auth_router = require('./routes/auth.routes')
 const api_router = require('./routes/api.routes')
 const auth_middleware = require('./middleware/auth.middleware')
 
+const PORT = process.env.WEBAPP_PORT || 3000
+
 require('./passport/passport')
 
-app.use(Cors())
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(passport.initialize())
@@ -27,4 +29,4 @@ app.use(
     }),
 )
 
-app.listen(4000, () => logger.info("Welcome to VITCMUN 2020"))
+app.listen(PORT, () => logger.info("Welcome to VITCMUN 2020"))
