28de833e3b5d7e58691301662191eff53fe4c735
Sid200026 <siddharthsingharoy@gmail.com>
2020-01-11 01:10:04 +0530

    feat(Country Selector) : Added Country Selector component

---
+++ client/src/components/CountryFlags.js
@@ -0,0 +1,102 @@
+import React, {useState} from 'react';
+import CountryNames from '../constants/CountryNames';
+import '../styling/CountryFlags.css';
+import FuzzySearch from 'fuzzy-search';
+import PropTypes from 'prop-types';
+
+// Recommended Width and Height
+// width: 355px;
+// height: 583px;
+
+// Pass a function as props to get the selected items
+
+// Country Names are used to filter countries and will be passed as selected from this component
+
+const CountryFlags = props => {
+    const [selected, setSelected] = useState(props.selectedArray)
+    const [keyword, setKeyword] = useState('');
+    const searcher = new FuzzySearch(CountryNames, ['name'], {
+        caseSensitive: false,
+        // A better matching result will be displayed first
+        sort: true,
+    })
+
+    // Add the country to selected or deselect it
+    const addRemoveIndex = (name) => {
+        let copy = [...selected];
+        if (copy.includes(name)) {
+            copy.splice(copy.indexOf(name),1)
+        } else {
+            copy.push(name);
+        }
+        setSelected(copy);
+    }
+
+    const clearAll = () => {
+        setSelected([])
+    }
+
+    const selectAll = () => {
+        let copy = []
+        countryImages = CountryNames.map(country => copy.push(country.name));
+        setSelected(copy);
+    }
+
+    // If country is selected then change display of that country
+    const getImage = (country) => {
+        if(selected.includes(country.name)) {
+            return <img src={country.source} className = 'Flag Selected' alt={country.name}/>
+        } else {
+            return <img src={country.source} className = 'Flag' alt={country.name}/>
+        }
+    }
+
+    const result = searcher.search(keyword);
+    let countryImages;
+    // If no matching result display some information
+    if(result.length === 0) {
+        countryImages = <b>No Such Country Exists</b>;
+    } else {
+        countryImages = result.map((country, index) =>
+            <div className="countryContainer" key={index}>
+                    <div onClick={() => addRemoveIndex(country.name)} className = 'CountryProfile'>
+                        {getImage(country)}
+                        <b className="countryName">{country.name}</b>
+                    </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className = 'CountriesMenu'>
+            <div className = 'CountriesMenuText'>
+                <b>UNHRC</b>
+                <div className="CountriesHeading">
+                    <b>Countries</b>
+                </div>
+                <div className="search">
+                    <input onSubmit={(event) => event.preventDefault()} placeholder="Enter the country" type="text" onChange={(event) => setKeyword(event.target.value)} ></input>
+                </div>
+            </div>  
+            <div className = 'CountryProfilesArea'>
+                {countryImages}
+            </div>  
+            <button className="selectorCountry" onClick={clearAll} type="button">Clear All</button>
+            <button className="selectorCountry" onClick={props.getSelected} type="button">Submit</button>
+            <button className="selectorCountry" onClick={selectAll} type="button">Select All</button>
+        </div>
+    );
+}
+
+// Validating the props passed
+
+CountryFlags.propTypes = {
+    selectedArray: PropTypes.shape([]),
+    getSelected: PropTypes.func.isRequired,
+}
+
+CountryFlags.defaultProps = {
+    selectedArray: [],
+}
+
+export {CountryFlags}
