0b032daf565dda6537bc0932e2c859ab1cd8f56f
Harshalshree <harshal.shree03@gmail.com>
2020-01-12 02:33:30 +0530

    fix() Merge issue

--- client/src/components/EBDashboard.jsx
+++ client/src/components/EBDashboard.jsx
@@ -2,28 +2,29 @@ import React from 'react';
 import './EBDashboard.css';
 
 const EBDashboard = props => {
-
   return (
     <div className="FlexContainer">
-
       <div className="LeftContainer">
-        <img className="MunImage1" src= "mun.png" alt="VITCC MUN"/>
-        <img className="CouncilImage" src= "unsc.png" alt="VITCC MUN"/>
+        <img className="MunImage1" src="mun.png" alt="VITCC MUN" />
+        <img className="CouncilImage" src="unsc.png" alt="VITCC MUN" />
       </div>
 
       <div className="RightContainer">
         <br></br>
-        <img className="MunImage2" src= "mun.png" alt="VITCC MUN"/>
-        <h4 className="DashboardHeading"><b>WELCOME EXECUTIVE BOARD!</b></h4><br/>
-        <div className = 'VotingButton'>
+        <img className="MunImage2" src="mun.png" alt="VITCC MUN" />
+        <h4 className="DashboardHeading">
+          <b>WELCOME EXECUTIVE BOARD!</b>
+        </h4>
+        <br />
+        <div className="VotingButton">
           <div className="Cardboard">
-            <img src="pollimg.jpg" alt="Poll" className = "PollImage"/>
+            <img src="pollimg.jpg" alt="Poll" className="PollImage" />
             <button className="Vote">Conduct Voting</button>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default EBDashboard;
