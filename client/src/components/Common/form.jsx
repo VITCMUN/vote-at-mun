0d2c4c710e7afbe537d1ce1cf03e3a3e3c00ddb4
Sarrthak <sarthak.trip99@gmail.com>
2020-02-09 03:30:41 +0530

    Login page added '%Y-%m-%d %H:%M:%S

--- client/src/components/Common/form.jsx
+++ client/src/components/Common/form.jsx
@@ -2,18 +2,19 @@ import React, { useState } from 'react';
 import '../../styling/login.css';
 
 const LoginForm = () => {
-    const [title, setTitle] = useState('');
-    const [password, setPassword] = useState('')
-    const handleSubmit = e => {
-        e.preventDefault();
-        setTitle('');
-        setPassword('');
-    }
-    return (
-    <div className="login-page">
-        <div className="form">
-            <form className="login-form" onSubmit={handleSubmit}>
-                <input type="text" value={title} placeholder="username" required onChange={ e => setTitle(e.target.value)}/>
+  const [title, setTitle] = useState('');    
+  const [password, setPassword] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    setTitle('');
+    setPassword('');
+};
+return (
+<div className="login-page">
+<div className="form">
+<form className="login-form" onSubmit={handleSubmit}>
+<input            type="text"            value={title}
+     placeholder="username" required onChange={ e => setTitle(e.target.value)}/>
                 <input type="password" value={password} placeholder="password" required onChange={e => setPassword(e.target.value)}/>
                 <button>login</button>
             </form>
