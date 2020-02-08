acfd650eda35251e5037d100773b6198f3757ed3
Sarrthak <sarthak.trip99@gmail.com>
2020-02-09 03:39:32 +0530

    Login page added (9/2/2020::3:39AM)

--- client/src/components/Common/login.jsx
+++ client/src/components/Common/login.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import './EBDashboard.css';
-import LoginForm from './form';  
+import React from "react";
+import "./EBDashboard.css";
+import LoginForm from "./form";
 
-function Login(){
+function Login() {
   return (
     <div className="FlexContainer">
-
       <div className="LeftContainer">
-        <img className="MunImage1" src= "mun.png" alt="VITCC MUN"/>
-        <img className="CouncilImage" src= "mun1.png" alt="VITCC MUN"/>
+        <img className="MunImage1" src="mun.png" alt="VITCC MUN" />
+        <img className="CouncilImage" src="mun1.png" alt="VITCC MUN" />
       </div>
 
       <div className="RightContainer">
         <br></br>
-        <img className="MunImage2" src= "mun.png" alt="VITCC MUN"/>
-        <h4 className="DashboardHeading"><b>WELCOME</b></h4><br/>
+        <img className="MunImage2" src="mun.png" alt="VITCC MUN" />
+        <h4 className="DashboardHeading">
+          <b>WELCOME</b>
+        </h4>
+        <br />
         <LoginForm />
       </div>
     </div>
