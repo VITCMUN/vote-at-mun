4e0876c6a0752b9e00256c9132e64c064d31c2ab
manak <manakpandey@gmail.com>
2020-03-01 22:30:58 +0530

    getActivePolls bug fix

--- server/src/graphql/resolvers/getActivePolls.resolver.js
+++ server/src/graphql/resolvers/getActivePolls.resolver.js
@@ -1,36 +1,39 @@
-const logger = require('../../winston');
-
-exports.getActivePolls = async (_, __, { currentUser ,Poll, Vote }) => {
+exports.getActivePolls = async (_, __, { currentUser, Poll, Vote }) => {
   const poll = await Poll.findAll({
     where: {
       active: true
     },
   });
 
-  logger.debug(poll)
+  const votes = await Vote.findAll({
+    where: {
+      voterId: currentUser.username
+    },
+    attributes: ['pollId']
+  });
+
+  const canVote = (pollId) => {
+    for (let i = 0; i < votes.length; i += 1) {
+      if (votes[i].pollId === pollId) return false;
+    }
+    return true;
+  };
 
   const polls = [];
 
   for (let i = 0; i < poll.length; i += 1) {
     const pollInfo = {};
-    pollInfo['pollId'] = poll[i]['id'];
-    pollInfo['title'] = poll[i]['title'];
-    pollInfo['description'] = poll[i]['description'];
-    pollInfo['totalSpeakerTime'] = poll[i]['total_speaker_time'];
-    pollInfo['raisedBy'] = poll[i]['raised_by'];
-    pollInfo['username'] = [];
-    if(currentUser){
-      const vote = await Vote.findAll({
-        where:{
-          pollId: pollInfo.pollId,
-          voterId: currentUser.username
-        }
-      })
-      if(vote.length==0)
+    pollInfo.pollId = poll[i].id;
+    pollInfo.title = poll[i].title;
+    pollInfo.description = poll[i].description;
+    pollInfo.totalSpeakerTime = poll[i].total_speaker_time;
+    pollInfo.raisedBy = poll[i].raised_by;
+    pollInfo.username = poll[i].username_filter;
+    if (currentUser) {
+      if (canVote(pollInfo.pollId)) {
         polls.push(pollInfo);
+      }
     }
-    else
-      polls.push(pollInfo)
   }
 
   return polls;
